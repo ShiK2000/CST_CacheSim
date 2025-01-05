d27862a6d6221a4ce7b356f18e19a087dafd59d3
Shik2000 <shirank@csl3.technion.ac.il>
2025-01-05 12:42:30 +0200

    misses are miscalculated but hey it compiles

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <cmath>
+#include <cmath> // literaly just for the pow and log function
 
 // fuck everything i am not recreating something that alredy exists
-
+const int DEBUG = 1;
 #include <vector>
 #include <map>
-#include <math.h> // literaly just for the pow function
 
 using std::cerr;
 using std::cout;
@@ -89,16 +88,21 @@ public:
 
 	unsigned long int RemoveLRU()
 	{
+		// not possible but just in case
+		if (this->elements.empty())
+		{
+			return -1;
+		}
 		unsigned long int deleted = *(this->elements.begin());
 		this->elements.erase(elements.begin());
 		return deleted;
 	}
 
-	bool removeSpecifically (unsigned long int fucker)
+	bool removeSpecifically(unsigned long int fucker)
 	{
 		for (std::vector<unsigned long>::iterator i = this->elements.begin(); i != this->elements.end(); i++)
 		{
-			if( (*i) == fucker)
+			if ((*i) == fucker)
 			{
 				this->elements.erase(i);
 				return true;
@@ -111,7 +115,7 @@ public:
 class Cache
 {
 private:
-	std::map<unsigned long int, Way*> sets; // assuming all accessed addresses are valid, we do not need to limit or check the inputs into here
+	std::map<unsigned long int, Way *> sets; // assuming all accessed addresses are valid, we do not need to limit or check the inputs into here
 	int numSets;
 	int setSize;
 	int associativity;
@@ -120,32 +124,31 @@ private:
 public:
 	Cache(unsigned long int size, int blockSize, int assoc, int wr_alloc)
 	{
+
 		wr_alloc = wr_alloc;
-		numSets = size / (blockSize * assoc);
-		setSize = size / assoc;
+		numSets = (assoc == 0 ? size : size / (blockSize * assoc));
+		setSize = (assoc == 0 ? size : size / assoc);
 		associativity = assoc;
-		sets = std::map<unsigned long int, Way*>();
+		sets = std::map<unsigned long int, Way *>();
 	}
 	~Cache()
 	{
-		for (std::map<unsigned long, Way *>::iterator i = sets.begin(); i != sets.end() ; i++)
+		for (std::map<unsigned long, Way *>::iterator i = sets.begin(); i != sets.end(); i++)
 		{
-			delete[] (*i).second;
+			delete (*i).second;
 		}
-		
 	}
 	bool affirmSetIsIn(unsigned long int x)
 	{
-		for (std::map<unsigned long, Way *>::iterator i = sets.begin(); i != sets.end() ; i++)
+		for (std::map<unsigned long, Way *>::iterator i = sets.begin(); i != sets.end(); i++)
 		{
-			if((*i).first == x)
+			if ((*i).first == x)
 			{
 				return true;
 			}
 		}
 		sets[x] = new Way(setSize);
-		return true;
-		
+		return sets[x] != NULL;
 	}
 	bool access(unsigned long int address, bool isWrite) {}
 
@@ -169,11 +172,11 @@ public:
 		return sets[set]->RemoveLRU();
 	}
 
-	bool removeSpecifically (unsigned long int fucker)
+	bool removeSpecifically(unsigned long int fucker)
 	{
-		for (std::map<unsigned long, Way*>::iterator i = sets.begin(); i != sets.end() ; i++)
+		for (std::map<unsigned long, Way *>::iterator i = sets.begin(); i != sets.end(); i++)
 		{
-			if( ((*i).second)->exists(fucker) )
+			if (((*i).second)->exists(fucker))
 			{
 				((*i).second)->removeSpecifically(fucker);
 				return true;
@@ -256,8 +259,7 @@ int main(int argc, char **argv)
 	}
 	// lovely ♥
 
-	int l1WaysSize = L1Size / L1Assoc;
-	int l2WaysSize = L2Size / L2Assoc;
+	// clearly till here we good
 
 	// it's supposed to kinda be an array/vector of the cache type, with each cell corresponding to way
 	// not vibing with vector<cache> as then we'll need to monitor it's size but cache[L1Size] also feels weird
@@ -283,7 +285,6 @@ int main(int argc, char **argv)
 	int L1acc = 0;
 	int L2acc = 0;
 
-
 	while (getline(file, line))
 	{
 
@@ -297,10 +298,6 @@ int main(int argc, char **argv)
 			return 0;
 		}
 
-		//
-		// DEBUG - remove this line
-		// cout << "operation: " << operation;
-
 		string cutAddress = address.substr(2); // Removing the "0x" part of the address
 		unsigned long int num = 0;
 		num = strtoul(cutAddress.c_str(), NULL, 16);
@@ -310,19 +307,18 @@ int main(int argc, char **argv)
 		// ok so we actually are looking only for top (size_of_num - block_size_in_log) digits
 		// so the cache is represented by:
 		num = num / (pow(10, BSize));
-		int setSize1 = std::log(L1Assoc);
-		int setSize2 = std::log(L2Assoc);
+		int setSize1 = L1Assoc;
+		int setSize2 = L2Assoc;
 
 		// the indexes for accessing the elements in the Ls
 		unsigned long int set1 = num % (unsigned long int)(pow(10, setSize1));
 		unsigned long int set2 = num % (unsigned long int)(pow(10, setSize2));
 
 		// if the sets aren't defined int the cache yes we should add them
-		// 	this is done automatically in the practical situation but i am trying to save us some 
+		// 	this is done automatically in the practical situation but i am trying to save us some
 		// 	space by NOT hacing thousands of arrays hanging around.
 		L1.affirmSetIsIn(set1);
 		L2.affirmSetIsIn(set2);
-
 
 		// L1 access happens always:
 		accTimeCounter += L1Cyc;
@@ -356,7 +352,7 @@ int main(int argc, char **argv)
 					// we need to add the line to L1 since it was used
 					if (!L1.add(set1, num))
 					{
-						// no space 
+						// no space
 						L1.RemoveLRU(set1);
 						L1.add(set1, num);
 					}
@@ -372,46 +368,45 @@ int main(int argc, char **argv)
 				// we must access memory
 				accTimeCounter += MemCyc;
 				// ok but at what price
-				if(operation == 'R' || !WrAlloc)
+				if (operation == 'R' || !WrAlloc)
 				{
 					// the (changed/)line goes into L2 and then L1
-					if(!L2.add(set2, num))
+					if (!L2.add(set2, num))
 					{
 						// no space in L2
 						// kick a bitch out
 						unsigned long int v = L2.RemoveLRU(set2);
 						L1.removeSpecifically(v); // we don't know what set it is from so just remove him if he's there
-						 
+
 						// space was freed. add
 						L2.add(set2, num);
 					}
 					// ok the element was added to L2
 					// now L1:
-					if(!L1.add(set1, num))
+					if (!L1.add(set1, num))
 					{
 						// no space in L1
 						// kicking a bitch out again
 						unsigned long int v = L1.RemoveLRU(set1);
-						
+
 						// space was freed. add
 						L1.add(set1, num);
 					}
 				}
-				//else
-					// no writing back, the change is only in memory and the cache is as oblivious as a three year old :)
-
+				// else
+				//  no writing back, the change is only in memory and the cache is as oblivious as a three year old :)
 			}
 
-		
+			if (DEBUG)
+				std::cout << "l1: " << L1miss << " L2: " << L2miss << std::endl;
 		}
 	}
-	L1MissRate = L1miss / L1acc;
-	L2MissRate = L2miss / L2acc;
-
+	L1MissRate = (double)L1miss / L1acc;
+	L2MissRate = (double)L2miss / L2acc;
 
 	double avgAccTime;
 	// we access l1 exactly once for each access we attempt
-	avgAccTime = accTimeCounter/L1acc; 
+	avgAccTime = accTimeCounter / L1acc;
 	// finally: printing
 	printf("L1miss=%.03f ", L1MissRate);
 	printf("L2miss=%.03f ", L2MissRate);
